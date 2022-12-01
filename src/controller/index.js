2f8df7413eed85ee89cfb5c538e497e2907329be
mindker <119412000+mindker@users.noreply.github.com>
2022-12-01 16:48:26 +0100

    feat: conexión establecida y getAllUsers funcionando
    * feat: INICIO BACKEND
    
    * feat: routes creada, controller creada
    
    * feat: conexion establecida y get all users funcionando
    
    Co-authored-by: JoseGomezLopez <fontanadeoro1983@gmail.com>
    Co-authored-by: Mario Muñoz P <113344127+Mamupe@users.noreply.github.com>
    Co-authored-by: Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>

---
+++ src/controller/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../domain/services/service-user');
+/* const decks = require('../domain/services/service-deck');
+const difficulties = require('../domain/services/service-user');
+const cards = require('../domain/services/service-card'); */
+
+router.get('/users', users.GetAll);
+
+module.exports = router;
