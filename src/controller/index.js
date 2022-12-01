db810782f264bcfb730e6770a2bc3925f42eacaf
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2022-12-01 16:30:33 +0100

    feat: conexion establecida y get all users funcionando

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../domain/services/service-user');
-const decks = require('../domain/services/service-deck');
-const difficulties = require('../domain/services/service-difficulty');
-const cards = require('../domain/services/service-card');
+/* const decks = require('../domain/services/service-deck');
+const difficulties = require('../domain/services/service-user');
+const cards = require('../domain/services/service-card'); */
 
 router.get('/users', users.GetAll);
+
+module.exports = router;
