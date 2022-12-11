f0113fb222e276795829ae84848bf61394accefd
Arrowdragonil <56029447+Arrowdragonil@users.noreply.github.com>
2022-12-11 21:11:03 +0100

    feat: añadrido required true en el entity-difficulty y en el services-difficulty añadido la idCard

--- src/domain/entities/entity-difficulty.js
+++ src/domain/entities/entity-difficulty.js
@@ -1,8 +1,8 @@
 module.exports = (db) => {
   const difficultySchema = new db.Schema(
     {
-      idCard: { type: db.Schema.Types.ObjectId, ref: 'Cards' },
-      idUser: { type: db.Schema.Types.ObjectId, ref: 'Users' },
+      idCard: { type: db.Schema.Types.ObjectId, ref: 'Cards', required: true },
+      idUser: { type: db.Schema.Types.ObjectId, ref: 'Users', required: true },
       level: { type: String, required: true }, // [enum_Difficulty]
     },
     {
